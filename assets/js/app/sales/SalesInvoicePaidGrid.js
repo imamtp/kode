88d08f846cdd4df38958f51e686c953b52718a8a
rachmat prabowo <praboworachmat@gmail.com>
2017-06-11 18:16:38 +0700

    add filter @ grid SI paid

--- assets/js/app/sales/SalesInvoicePaidGrid.js
+++ assets/js/app/sales/SalesInvoicePaidGrid.js
@@ -1,7 +1,7 @@
 Ext.define('SalesInvoicePaidGridModel', {
     extend: 'Ext.data.Model',
     fields: [
-       'idsales','no_sales_order','subtotal','freight','date_sales','tax','disc','totalamount','paidtoday','balance','comments','noinvoice','ddays','eomddays','percentagedisc','daydisc','notes_si','nocustomer','namecustomer','idpayment','invoice_status','invoice_date'
+        'idsales', 'no_sales_order', 'subtotal', 'freight', 'date_sales', 'tax', 'disc', 'totalamount', 'paidtoday', 'balance', 'comments', 'noinvoice', 'ddays', 'eomddays', 'percentagedisc', 'daydisc', 'notes_si', 'nocustomer', 'namecustomer', 'idpayment', 'invoice_status', 'invoice_date'
     ],
     idProperty: 'id'
 });
@@ -26,11 +26,14 @@ var storeGridSalesInvoicePaidGrid = Ext.create('Ext.data.Store', {
     }]
 });
 
-storeGridSalesInvoicePaidGrid.on('beforeload',function(store, operation,eOpts){
-       operation.params={
-                   'extraparams': 'a.invoice_status:'+2
-                 };
-             });
+storeGridSalesInvoicePaidGrid.on('beforeload', function(store, operation, eOpts) {
+    operation.params = {
+        'extraparams': 'a.invoice_status:' + 2 + ', ' +
+            'a.idunit:' + Ext.getCmp('idunit_grdsi_paid').getValue(),
+        'startdate': Ext.getCmp('startdate_grdsi_paid').getValue(),
+        'enddate': Ext.getCmp('enddate_grdsi_paid').getValue(),
+    };
+});
 
 Ext.define('MY.searchGridSalesInvoicePaidGrid', {
     extend: 'Ext.ux.form.SearchField',
@@ -54,7 +57,7 @@ var smGridSalesInvoicePaidGrid = Ext.create('Ext.selection.CheckboxModel', {
         }
     }
 });
-Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
+Ext.define(dir_sys + 'sales.SalesInvoicePaidGrid', {
     title: 'Paid',
     itemId: 'SalesInvoicePaidGrid',
     id: 'SalesInvoicePaidGrid',
@@ -63,115 +66,119 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
     store: storeGridSalesInvoicePaidGrid,
     loadMask: true,
     columns: [{
-        header: 'idsales',
-        dataIndex: 'idsales',
-        hidden: true
-    },{
-        header: 'No Sales',
-        dataIndex: 'no_sales_order',
-        hidden: true
-    },{
-        header: 'No Invoice',
-        dataIndex: 'noinvoice',
-        hidden: true
-    },{
-        header: 'Customer',
-        dataIndex: 'namecustomer',
-        minWidth: 150
-    },
-    {
-        header: 'Invoice Date',
-        dataIndex: 'invoice_date',
-        minWidth: 150
-    },
-    {
-        header: 'Sales Date',
-        dataIndex: 'date_sales',
-        minWidth: 150
-    },
-    {
-        header: 'Term Payment',
-        dataIndex: 'idpayment',
-        minWidth: 150,
-        renderer: function(value) {
-            return customColumnStatus(paymenttermarr,value);
-        }
-    },
-    {
-        header: 'Total Tax',
-        dataIndex: 'tax',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },
-    {
-        header: 'Total Discount',
-        dataIndex: 'disc',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },
-    {
-        header: 'Shipping Cost',
-        dataIndex: 'freight',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },
-    {
-        header: 'Total Amount',
-        dataIndex: 'totalamount',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },
-    {
-        header: 'Total Paid',
-        dataIndex: 'paidtoday',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },
-    {
-        header: 'Outstanding Payment',
-        dataIndex: 'balance',
-        minWidth: 150,xtype:'numbercolumn',align:'right'
-    },    
-    {
-        header: 'Status',
-        dataIndex: 'invoice_status',
-        minWidth: 150,xtype:'numbercolumn',align:'right',
-        renderer: function(value) {
-            return customColumnStatus(ArrInvoiceStatus,value);
-        }
-    },
+            header: 'idsales',
+            dataIndex: 'idsales',
+            hidden: true
+        }, {
+            header: 'No Sales',
+            dataIndex: 'no_sales_order',
+            hidden: true
+        }, {
+            header: 'No Invoice',
+            dataIndex: 'noinvoice',
+            hidden: true
+        }, {
+            header: 'Customer',
+            dataIndex: 'namecustomer',
+            minWidth: 150
+        },
+        {
+            header: 'Invoice Date',
+            dataIndex: 'invoice_date',
+            minWidth: 150
+        },
+        {
+            header: 'Sales Date',
+            dataIndex: 'date_sales',
+            minWidth: 150
+        },
+        {
+            header: 'Term Payment',
+            dataIndex: 'idpayment',
+            minWidth: 150,
+            renderer: function(value) {
+                return customColumnStatus(paymenttermarr, value);
+            }
+        },
+        {
+            header: 'Total Tax',
+            dataIndex: 'tax',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Total Discount',
+            dataIndex: 'disc',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Shipping Cost',
+            dataIndex: 'freight',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Total Amount',
+            dataIndex: 'totalamount',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Total Paid',
+            dataIndex: 'paidtoday',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Outstanding Payment',
+            dataIndex: 'balance',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right'
+        },
+        {
+            header: 'Status',
+            dataIndex: 'invoice_status',
+            minWidth: 150,
+            xtype: 'numbercolumn',
+            align: 'right',
+            renderer: function(value) {
+                return customColumnStatus(ArrInvoiceStatus, value);
+            }
+        },
     ],
-    dockedItems: [
-     {
-            xtype:'toolbar',
-            dock:'top',
-            items:[
-                {
-                    xtype: 'datefield',
-                    format: 'd/m/Y',
-                    // value: datenow(),
-                    fieldLabel: 'Invoice Period',
-                },
-                ' to ',
-                {
-                    xtype: 'datefield',
-                    format: 'd/m/Y',
-                    // value: datenow(),
-                    hideLabel:true
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: [{
+                xtype: 'datefield',
+                id: 'startdate_grdsi_paid',
+                format: 'd/m/Y',
+                // value: datenow(),
+                fieldLabel: 'Invoice Period',
+            },
+            ' to ',
+            {
+                xtype: 'datefield',
+                id: 'enddate_grdsi_paid',
+                format: 'd/m/Y',
+                // value: datenow(),
+                hideLabel: true
                     // fieldLabel: 'Date Order',
-                },'-',
-                {
-                    xtype:'comboxunit',
-                    valueField:'idunit',
-                    id:'cbSalesInvoicePaid',
-                    listeners: {
-                        'change': function(field, newValue, oldValue) {
-                            storeGridSalesInvoicePaidGrid.load({
-                                params: {
-                                  'extraparams': 'a.idunit:'+Ext.getCmp('cbSalesInvoicePaid').getValue()+','+'a.idanggotatype:'+Ext.getCmp('cbUnitPelangganType').getValue()
-
-                                }
-                            });
-                        }
-                    }
-                }
-            ]
-        },{
+            }, '-',
+            {
+                xtype: 'comboxunit',
+                valueField: 'idunit',
+                id: 'idunit_grdsi_paid',
+            }
+        ]
+    }, {
         xtype: 'toolbar',
         dock: 'top',
         items: [{
@@ -180,43 +187,52 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
             iconCls: 'print-icon',
             handler: function() {
                 var grid = Ext.ComponentQuery.query('SalesInvoicePaidGrid')[0];
-                 // var grid = Ext.getCmp('GridSalesInvoicePaidGridID');
+                // var grid = Ext.getCmp('GridSalesInvoicePaidGridID');
                 var selectedRecord = grid.getSelectionModel().getSelection()[0];
                 var data = grid.getSelectionModel().getSelection();
                 if (data.length == 0) {
                     Ext.Msg.alert('Failure', 'Pilih data terlebih dahulu!');
                 } else {
-                      
+
                     Ext.create('Ext.window.Window', {
                         title: 'Preview Invoice',
-                        modal:true,
-                        width: panelW-100,
-                        height: panelH-200,
-                        items: [
-                            {
-                                xtype: 'component',
-                                html: '<iframe src="' + SITE_URL + 'sales/print_invoice/' + selectedRecord.data.idsales + '"  style="position: absolute; border: 0; top:0; left:0; right:0; bottom:0; width:100%; height:100%;"></iframe>',
+                        modal: true,
+                        width: panelW - 100,
+                        height: panelH - 200,
+                        items: [{
+                            xtype: 'component',
+                            html: '<iframe src="' + SITE_URL + 'sales/print_invoice/' + selectedRecord.data.idsales + '"  style="position: absolute; border: 0; top:0; left:0; right:0; bottom:0; width:100%; height:100%;"></iframe>',
+                        }],
+                        buttons: [{
+                            text: 'Print',
+                            iconCls: 'print-icon',
+                            handler: function() {
+                                window.open(SITE_URL + 'sales/print_invoice/' + selectedRecord.data.idsales + '/print', '_blank');
                             }
-                        ],
-                        buttons: [
-                            {
-                                text: 'Print',
-                                iconCls: 'print-icon',
-                                handler: function () {
-                                    window.open(SITE_URL + 'sales/print_invoice/' + selectedRecord.data.idsales + '/print', '_blank');
-                                }
-                            }]
+                        }]
                     }).show();
                 }
             }
         }, {
+            text: 'Search',
+            handler: function() {
+                storeGridSalesInvoicePaidGrid.load();
+            }
+        }, {
+            text: 'Clear Filter',
+            handler: function() {
+                Ext.getCmp('startdate_grdsi_paid').setValue();
+                Ext.getCmp('enddate_grdsi_paid').setValue();
+                storeGridSalesInvoicePaidGrid.load();
+            }
+        }, {
             itemId: 'editSalesInvoicePaidGrid',
             text: 'Ubah',
-            hidden:true,
+            hidden: true,
             iconCls: 'edit-icon',
             handler: function() {
                 // var grid = Ext.ComponentQuery.query('GridSalesInvoicePaidGridID')[0];
-                 var grid = Ext.getCmp('GridSalesInvoicePaidGridID');
+                var grid = Ext.getCmp('GridSalesInvoicePaidGridID');
                 var selectedRecord = grid.getSelectionModel().getSelection()[0];
                 var data = grid.getSelectionModel().getSelection();
                 if (data.length == 0) {
@@ -228,7 +244,7 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
         }, {
             id: 'btnDeleteSalesInvoicePaidGrid',
             text: 'Hapus',
-            hidden:true,
+            hidden: true,
             iconCls: 'delete-icon',
             handler: function() {
                 Ext.Msg.show({
@@ -248,7 +264,7 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
                                 method: 'POST',
                                 params: {
                                     postdata: Ext.encode(selected),
-                                    idmenu:95
+                                    idmenu: 95
                                 },
                                 success: function(form, action) {
                                     var d = Ext.decode(form.responseText);
@@ -275,7 +291,7 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
         store: storeGridSalesInvoicePaidGrid, // same store GridPanel is using
         dock: 'bottom',
         displayInfo: true
-        // pageSize:20
+            // pageSize:20
     }],
     listeners: {
         render: {
@@ -286,49 +302,48 @@ Ext.define(dir_sys+'sales.SalesInvoicePaidGrid', {
             }
         },
         itemdblclick: function(dv, record, item, index, e) {
-           // showDataSales(record.data.idsales)
+            // showDataSales(record.data.idsales)
         }
     }
 });
 
 
-function loadMemberForm(id)
-{
+function loadMemberForm(id) {
     // anggotaTypeStore.load();
 
-        // var formSalesInvoicePaidGrid = Ext.getCmp('formSalesInvoicePaidGrid');
-        // wSalesInvoicePaidGrid.show();
-        // formSalesInvoicePaidGrid.getForm().load({
-        //     url: SITE_URL + 'backend/loadFormData/SalesInvoicePaidGrid/1/member',
-        //     params: {
-        //         extraparams: 'a.id_member:' + id
-        //     },
-        //     success: function(form, action) {
-        //         var obj = Ext.decode(action.response.responseText); 
-        //         // console.log(obj);
-        //         Ext.getCmp('comboxStatusMember').setValue(obj.data.status*1);
-        //         formSalesInvoicePaidGrid.getForm().findField("id_member_type").setValue(obj.data.id_member_type);
-        //         formSalesInvoicePaidGrid.getForm().findField("marital_status").setValue(obj.data.marital_status*1);
-        //         // Ext.Msg.alert("Load failed", action.result.errorMessage);
-        //     },
-        //     failure: function(form, action) {
-        //         Ext.Msg.alert("Load failed", action.result.errorMessage);
-        //     }
-        // })
+    // var formSalesInvoicePaidGrid = Ext.getCmp('formSalesInvoicePaidGrid');
+    // wSalesInvoicePaidGrid.show();
+    // formSalesInvoicePaidGrid.getForm().load({
+    //     url: SITE_URL + 'backend/loadFormData/SalesInvoicePaidGrid/1/member',
+    //     params: {
+    //         extraparams: 'a.id_member:' + id
+    //     },
+    //     success: function(form, action) {
+    //         var obj = Ext.decode(action.response.responseText); 
+    //         // console.log(obj);
+    //         Ext.getCmp('comboxStatusMember').setValue(obj.data.status*1);
+    //         formSalesInvoicePaidGrid.getForm().findField("id_member_type").setValue(obj.data.id_member_type);
+    //         formSalesInvoicePaidGrid.getForm().findField("marital_status").setValue(obj.data.marital_status*1);
+    //         // Ext.Msg.alert("Load failed", action.result.errorMessage);
+    //     },
+    //     failure: function(form, action) {
+    //         Ext.Msg.alert("Load failed", action.result.errorMessage);
+    //     }
+    // })
 
-        // Ext.getCmp('memberFormDetailID').getForm().load({
-        //     url: SITE_URL + 'backend/loadFormData/SalesInvoicePaidGrid/1/member',
-        //     params: {
-        //         extraparams: 'a.id_member:' + id
-        //     },
-        //     success: function(form, action) {
-        //         // Ext.Msg.alert("Load failed", action.result.errorMessage);
-        //     },
-        //     failure: function(form, action) {
-        //         Ext.Msg.alert("Load failed", action.result.errorMessage);
-        //     }
-        // })
+    // Ext.getCmp('memberFormDetailID').getForm().load({
+    //     url: SITE_URL + 'backend/loadFormData/SalesInvoicePaidGrid/1/member',
+    //     params: {
+    //         extraparams: 'a.id_member:' + id
+    //     },
+    //     success: function(form, action) {
+    //         // Ext.Msg.alert("Load failed", action.result.errorMessage);
+    //     },
+    //     failure: function(form, action) {
+    //         Ext.Msg.alert("Load failed", action.result.errorMessage);
+    //     }
+    // })
 
-        // Ext.getCmp('statusformSalesInvoicePaidGrid').setValue('edit');
-        // Ext.getCmp('Tabanggota').setActiveTab(0);
+    // Ext.getCmp('statusformSalesInvoicePaidGrid').setValue('edit');
+    // Ext.getCmp('Tabanggota').setActiveTab(0);
 }
