e63bbc25355ea1093674d0da7a9356facb5df401
rachmat prabowo <praboworachmat@gmail.com>
2017-08-16 15:06:53 +0700

    set list PO utk menampilkan po dg status open, ordered, dan partialy received

--- application/models/purchase/m_purchaseorder.php
+++ application/models/purchase/m_purchaseorder.php
@@ -91,7 +91,7 @@ class m_purchaseorder extends CI_Model {
     function whereQuery() {
          $wer = null;
         if($this->input->post('option')=='notyetdelivered'){
-            $wer = " and a.delivereddate is null and a.idpurchasestatus = 2";
+            $wer = " and a.idpurchasestatus in (2,3,5)";
         } else if($this->input->post('option')=='po_not_in_open_return_status'){
             //po tidak sedang dalam retur dan belum dibuatkan invoicenya
             $wer = " and a.idpurchase not in (select idpurchase from purchase_return where (return_status = 1 OR return_status = 3 OR return_status = 4) ) and a.delivereddate is not null";
