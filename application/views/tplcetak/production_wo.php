e89d220ffb46cd9669c2ce688149b6f35cfcf6e3
rachmat prabowo <praboworachmat@gmail.com>
2017-08-23 15:55:38 +0700

    fixing issue with layout wo articles

--- application/views/tplcetak/production_wo.php
+++ application/views/tplcetak/production_wo.php
@@ -8,97 +8,66 @@
     <title><?=$title?></title>
     <link rel="stylesheet" href="<?=base_url()?>/assets/css/bootstrap.min.css">
     <link href="<?=base_url()?>/assets/css/print.css" rel="stylesheet">
-
-    <style>
-   
-    </style>
 </head>
 
-<?php
-if($print) { echo "<body onload=\"window.print()\">"; } else { echo "<body>"; }
-?>
-
+<body>
     <div class="container">
-
       <div class="panel panel-info">
       <div class="panel-body">
-      
-
     
       <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-5">
           <h1>
-            <!-- <img src="<?=base_url()?>/assets/images/<?=$data['logo']?>" width="200"/> -->
             <?=$this->logo?>
           </h1>
         </div>
-        <div class="col-xs-6 text-right">
-          <h2><?=$title?></h2>
-          <h3><small>NO: #<?=$data['header']['job_no']?><br>Harus Selesai Tanggal: <?=backdate2($data['header']['req_ship_date'])?><br></small></h3>
+        <div class="col-xs-5 col-xs-offset-2 text-right">
+          <h3><?=$title?></h3>
+          <h4><small>NO: #<?=$data['header']['job_no']?><br>Harus Selesai Tanggal: <?=backdate2($data['header']['req_ship_date'])?><br></small></h4>
         </div>
       </div>
       
       <div class="row" style="margin-left:1px;">
         <table class="table borderless" >
-        
-         <tr>
-           <td width="22%"><b>Finished Goods List:</b></td>
-           <td width="50%"></td>
-         </tr>
-         <?php
-         // echo $data['totaltax'];
-
-              if(count($data['fg_list'])>0)
-              {
-              ?>
-                      <table class="table table-bordered" style="width:99%; margin-left:1px; margin-right:2px;">
-                        <tr>
-                          <th width="30">No</th>  
-                          <th>No SKU</th>                       
-                          <th>Nama Barang</th>    
-                          <th>Qty</th>                 
-                          <th>Satuan Qty</th>
-                          <th>Ukuran</th>
-                          <th>Satuan Ukuran</th>
-                          <th>Raw Material</th>
-                          <th>Kode  Barang</th>
-                          <th>Qty</th>
-                          <th>Satuan Qty</th>
-                          <th>Material Terpakai</th>
-                          <th>Keterangan</th>
-                        </tr>
-                        <?php
-                        // print_r($data['detail']);
-                        $i=1;
-                        foreach ($data['fg_list'] as $key => $value) {
-                           ?>
-                             <tr>
-                              <td width="30"><?=$i?></td>
-                              <td><?=$value['sku_no']?></td>
-                              <td><?=$value['nameinventory']?></td> 
-                              <td><?=$value['qty']?></td>   
-                              <td><?=$value['short_desc']?></td>  
-                              <td><?=$value['size']?></td>  
-                              <td><?=$value['size_measurement']?></td>  
-                              <td><?=$value['rm_list'][0]['invno']?></td>  
-                              <td><?=$value['rm_list'][0]['nameinventory']?></td>
-                              <td><?=$value['rm_list'][0]['qty']?></td>  
-                              <td><?=$value['rm_list'][0]['measurement_name']?></td>
-                              <td>&nbsp;</td>
-                              <td>&nbsp;</td>
-                            </tr>
-                           <?php
-                         
-                           $i++;
-                          }
-                        ?>
-                       
-                      </table>
-              <?php
-              }
-              ?>
-
-
+          <tr>
+            <td colspan="2"><b>Finished Goods List:</b></td>
+          </tr>
+          <?php if(count($data['fg_list'])>0) : ?>
+            <table class="table table-bordered" style="width:99%; margin-left:1px; margin-right:2px;">
+              <tr>
+                <th width="30">No</th>  
+                <th>No SKU</th>                       
+                <th>Nama Barang</th>    
+                <th>Qty</th>                 
+                <th>Satuan Qty</th>
+                <th>Ukuran</th>
+                <th>Satuan Ukuran</th>
+                <th>Raw Material</th>
+                <th>Kode  Barang</th>
+                <th>Qty</th>
+                <th>Satuan Qty</th>
+                <th>Material Terpakai</th>
+                <th>Keterangan</th>
+              </tr>
+          <?php foreach ($data['fg_list'] as $key => $value): ?>
+              <tr>
+              <td width="30"><?=$key+1?></td>
+              <td><?=$value['sku_no']?></td>
+              <td><?=$value['nameinventory']?></td> 
+              <td><?=$value['qty']?></td>   
+              <td><?=$value['short_desc']?></td>  
+              <td><?=$value['size']?></td>  
+              <td><?=$value['size_measurement']?></td>  
+              <td><?=$value['rm_list'][0]['invno']?></td>  
+              <td><?=$value['rm_list'][0]['nameinventory']?></td>
+              <td><?=$value['rm_list'][0]['qty']?></td>  
+              <td><?=$value['rm_list'][0]['measurement_name']?></td>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>
+          <?php endforeach; ?>
+            </table>
+          <?php endif; ?>
         </table>
       </div>
 
@@ -132,11 +101,8 @@ if($print) { echo "<body onload=\"window.print()\">"; } else { echo "<body>"; }
         </div>
       </div>
 
-          </div>
-  </div> <!-- panel -->
-  
-
-    </div><!-- container -->
-      
-  </body>
+    </div>
+    </div> <!-- panel -->
+  </div><!-- container -->
+</body>
 </html>
