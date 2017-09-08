3ed09d2b61f398bb73e5d8e15e0d6dfbda4ee521
Rachmat Prabowo <praboworachmat@gmail.com>
2017-09-08 12:58:37 +0700

    merge dev_inventory to master

--- assets/js/app/inventory/windowGridDetailInventory.js
+++ assets/js/app/inventory/windowGridDetailInventory.js
@@ -5,7 +5,7 @@
 //               });
 Ext.define('GridItemGridDetailInventoryModel', {
     extend: 'Ext.data.Model',
-    fields: ['sku_no', 'nameinventory', 'invno', 'hpp','notes', 'idinventory', 'cost', 'stock_one', 'uom_one', 'stock_two', 'uom_two', 'stock_tre', 'uom_tre', 'warehouse_code', 'no_batch', 'received_date'],
+    fields: ['sku_no', 'nameinventory', 'invno', 'hpp', 'notes', 'idinventory', 'cost', 'stock_one', 'uom_one', 'stock_two', 'uom_two', 'stock_tre', 'uom_tre', 'warehouse_code', 'no_batch', 'received_date'],
     // fields: ['idinventory', 'totalitem', 'sku_no', 'satuan_pertama', 'invno', 'nameinventory', 'description', 'isinventory', 'issell', 'isbuy', 'cosaccount',
     // 'incomeaccount', 'assetaccount', 'qtystock', 'images', 'cost', 'unitmeasure', 'numperunit', 'minstock', 'idprimarysupplier',
     // 'sellingprice', 'idselingtax', 'unitmeasuresell', 'numperunitsell', 'notes', 'display', 'namesupplier', 'yearbuy', 'monthbuy', 'datebuy', 'namaunit', 'brand_name', 'brand_id', 'sku', 'totalstock', 'stock_kedua', 'satuan_kedua'
@@ -167,18 +167,10 @@ Ext.define('GridItemGridDetailInventory', {
             minWidth: 100
         },
         {
-<<<<<<< HEAD
             header: 'Harga Beli',
             dataIndex: 'cost',
             xtype: 'numbercolumn',
             align: 'right',
-=======
-            header: 'HPP',
-            xtype: 'numbercolumn',
-            minWidth: 120,
-            align: 'right',
-            dataIndex: 'hpp',
->>>>>>> 3232d8af21c05819faa6e04c77d4c5c0f54f0b12
         },
         {
             header: 'Kode Gudang',
