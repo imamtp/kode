38472b50ff11b6d21202e3f96c8c9eb8eb158d00
Rachmat Prabowo <praboworachmat@gmail.com>
2017-10-05 12:36:33 +0700

    fixing sales print

--- application/views/tplcetak/sales_print.php
+++ application/views/tplcetak/sales_print.php
@@ -77,25 +77,24 @@
 		<?php endforeach; ?>
 		</tbody>
 	</table>	 
-
-	<table class="table" border="1" cellpadding="0" cellspacing="0" style="page-break-inside:avoid">
+	<table class="table" border="0" cellpadding="0" cellspacing="0" style="page-break-inside:avoid">
 		<tr>
 			<td width="55%"></td>
-			<td width="13%"></td>
+			<td width="15%"></td>
 			<td width="2%"></td>
-			<td width="20%"></td>
+			<td width="18%"></td>
 			<td width="10%"></td>
 		</tr>
 		<tr>
-			<td width="50%" ></td>
-			<td width="5%" align="left">Subtotal </td>
-			<td width="2%">:</td>
+			<td></td>
+			<td align="left">Subtotal </td>
+			<td>:</td>
 			<td colspan="2" align="right">
 					<?=$data['detailtotal']?>
 			</td>
 		</tr>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">DPP </td>
 			<td>:</td>
 			<td align="right">
@@ -104,7 +103,7 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">PPN </td>
 			<td>:</td>
 			<td align="right">
@@ -113,7 +112,7 @@
 			<td></td>
 		</tr>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">Biaya Kirim </td>
 			<td>:</td>
 			<td></td>
@@ -122,7 +121,7 @@
 			</td>
 		</tr>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">Grand Total </td>
 			<td>:</td>
 			<td colspan="2" align="right">
@@ -131,7 +130,7 @@
 		</tr>
 		<?php if($data['totalowed']=='9x0'): ?>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">Saldo Terhutang </td>
 			<td>:</td>
 			<td colspan="2" align="right">
@@ -140,35 +139,33 @@
 		</tr>
 		<?php endif; ?>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td align="left">Terbilang </td>
 			<td>:</td>
 			<td colspan="2" align="left"></td>
 		</tr>
 		<tr>
-			<td width="45%"></td>
+			<td></td>
 			<td colspan="4" align="left"><?=$data['terbilang']?></td>
 		</tr>
+		<tr><td colspan="7">&nbsp;</td></tr>
 		<tr>
-			<td width="45%"></td>
-			<td colspan="4" align="right" style="text-decoration:underline;">Payment Term: <?=$data['payment_term']?></td>
+			<td class="cell-bordered"><h3>Ship Address</h3></td>
+			<!-- <td rowspan="2"></td> -->
+			<td colspan="4" style="text-decoration:underline;" align="right">Payment Term: <?=$data['payment_term']?></td>
 		</tr>
+		<tr>
+			<td class="cell-bordered"><?=$data['shipaddress']?></td>
+		</tr>
+		<tr><td></td></tr>
+		<tr>
 	</table>
 	<br>
 
 	<table class="table" border="0" cellpadding="0" cellspacing="0" style="page-break-inside:avoid">
 		<tr>
-			<td class="cell-bordered" width="49%"><h3>Ship Address</h3></td>
-			<td rowspan="4" width="2%"></td>
-		</tr>
-		<tr>
-			<td class="cell-bordered"><?=$data['shipaddress']?></td>
-			<td colspan="5">&nbsp;</td>
-		</tr>
-		<tr><td colspan="7">&nbsp;</td></tr>
-		<tr>
 			<!-- notes -->
-			<td>
+			<td width="45%">
 				<p>Notes:</p>
 				<ul>
 				<?php 
@@ -180,14 +177,16 @@
 				</ul>
 			</td>
 			<!-- end of notes -->
+			
+			<td></td>
 
 			<!-- ttd 1 -->
-			<td colspan="3" align="center">
+			<td width="25%" align="center">
 				Disetujui oleh:<br><br><br><br><br><br><br>(<?=$data['customer']['namecustomer']?>)</td>
 			<!-- end of ttd1 -->
 
 			<!-- ttd 2 -->
-			<td colspan="2" align="center">
+			<td width="25%" align="center">
 				<br><br><br><br><br><br><br>(Ivan Susanto)
 			</td>
 			<!-- end of ttd 2 -->
